ad3e6c7f22a417b966c6666f714c2b925248d2d9
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2024-04-26 17:54:31 +0100

    started the server

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"net/http"
 	"os"
 
 	"github.com/joho/godotenv"
@@ -9,4 +10,28 @@ import (
 func main() {
 	godotenv.Load()
 	port := os.Getenv("PORT")
+
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+	cors := middlewareCors(mux)
+
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: cors,
+	}
+
+	server.ListenAndServe()
+}
+
+func middlewareCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS,PUT,DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		if r.Method == "Options" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
