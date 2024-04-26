b89bfd0c80fa6873513b0b425034d7c71f8d61c6
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2024-04-26 18:34:46 +0100

    deleted headers from main file

--- main.go
+++ main.go
@@ -22,16 +22,3 @@ func main() {
 
 	server.ListenAndServe()
 }
-
-func middlewareCors(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS,PUT,DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "*")
-		if r.Method == "Options" {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-		next.ServeHTTP(w, r)
-	})
-}
